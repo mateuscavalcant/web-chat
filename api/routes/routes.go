8dbb2dc70e152eb872c7d84207d7aa781d41de52
Mateus <mateuscavalcant7@gmail.com>
2024-02-26 15:15:19 -0300

    Deleted Templates HTML

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 	"web-chat/api/handlers"
 	"web-chat/api/middlewares"
-	"web-chat/api/views"
 
 	"github.com/gorilla/mux"
 )
@@ -12,12 +11,9 @@ import (
 func InitRoutes(r *mux.Router) {
 	// Initialize the handlers
 	r.HandleFunc("/signup", handlers.CreateUserAccount).Methods("POST")
-	r.HandleFunc("/login", views.RenderLoginTemplate).Methods("GET")
 	r.Handle("/login", middlewares.LimitLoginAttempts(http.HandlerFunc(handlers.AccessUserAccount)))
 	r.HandleFunc("/chat/{username}", handlers.Chat).Methods("POST")
 	r.HandleFunc("/create-message/{username}", handlers.CreateNewMessage).Methods("POST")
 	r.HandleFunc("/websocket/{username}", handlers.WebSocketHandler)
-	r.Handle("/home", middlewares.AuthMiddleware(http.HandlerFunc(views.RenderHomeTemplate))).Methods("GET")
-	r.Handle("/chat/{username}", middlewares.AuthMiddleware(http.HandlerFunc(views.RenderChatTemplate))).Methods("GET")
 }
 
